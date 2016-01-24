9529e0e74874da80204afb1708b0e215845bc664
Sheng <nstop@foxmail.com>
2016-01-24 19:24:56 +0800

    commandline test

---
+++ dwtime.py
@@ -0,0 +1,7 @@
+# caculate the time to download the file
+
+
+
+file_size = raw_input("Please input the size of the file you want to download:") 
+
+download_speed = raw_input("Please input your speed of downloading(KB/S or MB/S):")
